f90f24489bad68e1a28052205da6a40a5506f547
Ben Crist <github@magicmoremagic.com>
2025-02-13 20:14:58 -0600

    fix zig 0.14 enum renaming

--- src/Fuse_Range.zig
+++ src/Fuse_Range.zig
@@ -17,7 +17,7 @@ pub fn init(what: anytype) Fuse_Range {
         Fuse_Range => what,
         JEDEC_Data => what.extents,
         else => switch (@typeInfo(T)) {
-            .pointer => |ptr_info| if (ptr_info.size == .One) switch (ptr_info.child) {
+            .pointer => |ptr_info| if (ptr_info.size == .one) switch (ptr_info.child) {
                 device.Type => what.get().jedec_dimensions,
                 Fuse => .{ .min = what.*, .max = what.* },
                 Fuse_Range => what.*,
